e6cf1e58d9ac2ad0cdbac464ca1bbb370d9737fc
Brandon Wong <brandon.wong@seagate.com>
2016-09-22 15:50:44 -0500

    got textarea to be wired up with javascript

--- parse.js
+++ parse.js
@@ -1,4 +1,21 @@
-var esprima = require('acorn');
-//var x = document.getElementById("myTextarea").value;
-var test = esprima.parse('var i = 0;');
-console.log(JSON.stringify(test, null, 4));
+var acorn = require('acorn');
+
+//console.log(JSON.stringify(test, null, 4));
+
+function test()
+{
+    console.log("hi");
+}
+
+
+function displayParsed()
+{
+    var x = document.getElementById("myTextarea").value;
+    var test = acorn.parse(x);
+    document.getElementById("demo").innerHTML=JSON.stringify(test, null, 2);
+}
+
+window.onload = function() {
+    var btn = document.getElementById("myButton");
+    btn.onclick = displayParsed;
+}
