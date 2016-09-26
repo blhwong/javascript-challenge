e214a4a6840b27d93e85b7b3322e9ab10d390844
Brandon Wong <brandon.wong@seagate.com>
2016-09-26 16:01:10 -0500

    implementing traversing AST

--- parse.js
+++ parse.js
@@ -1,21 +1,108 @@
 var acorn = require('acorn');
+var walk = require('acorn/dist/walk');
 
-//console.log(JSON.stringify(test, null, 4));
-
-function test()
+var whiteList =
 {
-    console.log("hi");
-}
+    "VariableDeclaration":false,
+    "ForStatement":false
+};
 
-
-function displayParsed()
+var blackList =
 {
+    "WhileStatement":false,
+    "IfStatement":false
+};
+
+function getResults()
+{
+    for(var i in whiteList)
+    {
+        whiteList[i] = false;
+    }
+    for(var j in blackList)
+    {
+        blackList[j] = false;
+    }
+    //console.log(whiteList, blackList);
     var x = document.getElementById("myTextarea").value;
-    var test = acorn.parse(x);
-    document.getElementById("demo").innerHTML=JSON.stringify(test, null, 2);
+    var ast = acorn.parse(x);
+    walk.simple(ast, { 
+        Function: function(node){
+            console.log(node, "Function");},
+        Statement: function(node){
+            console.log(node, "Statement");}
+    });
+    /*
+    traverse(ast.body);
+    var whiteListResult = "";
+    for(var i in whiteList)
+    {
+        if(!whiteList[i])
+        {
+            if(!whiteListResult)
+            {
+                whiteListResult = "This program MUST use a " + i;
+            }
+            else
+            {
+                whiteListResult += " and a " + i;
+            }
+        }
+    }
+    whiteListResult +='.';
+    document.getElementById("whitelist").innerHTML=whiteListResult;
+
+    var blackListResult = "";
+    for(var i in blackList)
+    {
+        if(blackList[i])
+        {
+            if(!blackListResult)
+            {
+                blackListResult = "This program MUST NOT use a " + i;
+            }
+            else
+            {
+                blackListResult += " or a " + i;
+            }
+        }
+    }
+    blackListResult +='.';
+    document.getElementById("blacklist").innerHTML=blackListResult;
+    */
+    document.getElementById("parsed").innerHTML=JSON.stringify(ast, null, 2);
 }
 
-window.onload = function() {
+function traverse(array)
+{
+    for(var key in array)
+    {
+        console.log(array[key]);
+        if(whiteList.hasOwnProperty(array[key].type))
+        {
+            //console.log("Found White list Item");
+            if(!whiteList[array[key].type])
+            {
+                //console.log("Changing to true");
+                whiteList[array[key].type] = true;
+            }
+            //console.log(whiteList);
+        }
+        if(blackList.hasOwnProperty(array[key].type))
+        {
+            //console.log("Found Black list Item");
+            if(!blackList[array[key].type])
+            {
+                //console.log("Changing to true");
+                blackList[array[key].type] = true;
+            }
+            //console.log(blackList);
+        }
+    }
+}
+
+window.onload = function()
+{
     var btn = document.getElementById("myButton");
-    btn.onclick = displayParsed;
+    btn.onclick = getResults;
 }
