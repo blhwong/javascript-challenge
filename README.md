63777063ca82c98c34131ac60a2b3cfc082030ee
Brandon Wong <brandon.wong@seagate.com>
2016-09-29 10:16:08 -0500

    added comments and created readme file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # javascript-challenge
+Author: Brandon Wong
+Date Modified: 9/29/2016
+
+This is a Javascript framework project to detect nodes within whitelist,
+blacklist, and the general structure of program.
+
+parse.js is my original code and I used browserify to create bundle.js
+so that require functions are working for acorn and estree-walker.
+
+Create more whiteList and blackList elements by creating more node types
+following the estree spec within the whiteList and blackList objects under 
+parse.js and setting the elements to false. 
+e.g. 
+var whiteList =
+{
+    "VariableDeclaration":false,
+    "ForStatement":false
+};
+
+Make sure it is compiled within browserify since html file is linked with
+bundle.js
+
+Open index.html in browser to run framework. CodeMirror was used to wire
+my textarea. Add code within text editor and hit submit to view results.
--- parse.js
+++ parse.js
@@ -1,18 +1,28 @@
 var acorn = require('acorn');
 var walk = require('estree-walker').walk;
 
+/*
+add more items to whiteList by creating Node types and setting them to false.
+*/
 var whiteList =
 {
     "VariableDeclaration":false,
     "ForStatement":false
 };
 
+/*
+add more items to blackList by creating Node types and setting them to false.
+*/
 var blackList =
 {
     "WhileStatement":false,
     "IfStatement":false
 };
 
+/*
+add more items to wantedNodeTypes so that the walker function traverses
+through the relevant Nodes.
+*/
 var wantedNodeTypes =
 {
     "Functions":true,
@@ -26,19 +36,27 @@ var wantedNodeTypes =
     "VariableDeclaration":true
 }
 
+/*
+Checks the top of the stack
+*/
 function peek(array)
 {
     return array[array.length -1];
 }
 
+/*
+Gets results of whiteList, blackList, and structure.
+*/
 function getResults()
 {
     for(var i in whiteList)
     {
+        //clears whiteList previously detected
         whiteList[i] = false;
     }
     for(var j in blackList)
     {
+        //clears blackList previously detected
         blackList[j] = false;
     }
     var endStack = [];
@@ -50,18 +68,23 @@ function getResults()
       {
           if(!wantedNodeTypes[node.type])
           {
+              //skips nodes that aren't wanted
               return;
           }
           if(whiteList.hasOwnProperty(node.type) && !whiteList[node.type])
           {
+              //whiteList node detected!
               whiteList[node.type] = true;
           }
           if(blackList.hasOwnProperty(node.type) && !blackList[node.type])
           {
+              //blackList node detected!
               blackList[node.type] = true;
           }
+          //structure algorithm begins here
           if(!structureResult)
           {
+              //this is the first node in the structure detected!
               structureResult = "This program starts with a " + node.type;
               endStack.push([node.end, node.type]);
           }
@@ -69,25 +92,31 @@ function getResults()
           {
               if(node.start > peek(endStack)[0])
               {
+                  //if the current start position of node is greater than
+                  //the top of the stack then we need to continue to next node
                   while(endStack.length > 0 && node.start > peek(endStack)[0])
                   {
+                      //pops nodes off the stack that are previous to current node
                       endStack.pop();
                   }
                   if(endStack.length > 0)
                   {
+                      //if the current node is still within a node
                       structureResult += ". Continuing within " + peek(endStack)[1] + ", there is a " + node.type;
                   }
                   else
                   {
+                      //if the current node is not within a node
                       structureResult += ". Next, there is a " + node.type;
                   }
                   endStack.push([node.end, node.type]);
               }
               else
               {
-                  //go inside
+                  //need to traverse inside the node
                   if(parent.type == "ForStatement" && node.type == "VariableDeclaration")
                   {
+                      //ignores variable delecration within forloop control
                       return;
                   }
                   structureResult += ", and inside there is a " + node.type;
@@ -98,56 +127,68 @@ function getResults()
     });
     if(structureResult)
     {
+        //if there was a result then concat a period
         structureResult+= '.';
     }
     else
     {
-        structureResult = "This program is empty";
+        //result was blank
+        structureResult = "This program is empty.";
     }
     var whiteListResult = "";
     for(var i in whiteList)
     {
+        //iterate whiteList to detect those unused
         if(!whiteList[i])
         {
             if(!whiteListResult)
             {
+                //first whiteList element was not used
                 whiteListResult = "This program MUST use a " + i;
             }
             else
             {
+                //subsequent whiteList elements not used
                 whiteListResult += " and a " + i;
             }
         }
     }
     if(whiteListResult)
     {
+        //add period if result is not empty
         whiteListResult +='.';
     }
     else
     {
+        //all whiteList elements were used
         whiteListResult = "is good."
     }
     var blackListResult = "";
     for(var i in blackList)
     {
+        //iterate whiteList to detect those used
         if(blackList[i])
         {
             if(!blackListResult)
             {
+                //first blackList element used
                 blackListResult = "This program MUST NOT use a " + i;
             }
             else
             {
+                //subsequent blackList elements used
                 blackListResult += " or a " + i;
             }
         }
     }
     if(blackListResult)
     {
+        //add period if result is not empty.
         blackListResult +='.';
     }
     else
     {
+        //all blackList elements were unused
         blackListResult = "is good."
     }
     document.getElementById("whitelist").innerHTML="1. (Whitelist) " + whiteListResult;
